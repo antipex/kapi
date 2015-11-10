669fe907fc52eb885c19423ccbd9b669957093f1
antipex <kyle@antipex.com>
2015-11-10 12:25:37 -0800

    Switched to class based views

--- foo/urls.py
+++ foo/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from foo import views
 
 urlpatterns = [
-    url(r'^foo/$', views.foo_list),
-    url(r'^foo/(?P<pk>[0-9]+)/$', views.foo_detail),
+    url(r'^foo/$', views.FooList.as_view()),
+    url(r'^foo/(?P<pk>[0-9]+)/$', views.FooDetail.as_view()),
 ]
--- foo/views.py
+++ foo/views.py
@@ -1,10 +1,14 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from foo.models import Foo
 from foo.serializers import FooSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class JSONResponse(HttpResponse):
@@ -17,48 +21,46 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-@csrf_exempt
-def foo_list(request):
+class FooList(APIView):
     """
     List all foos or create a new foo.
     """
-
-    if request.method == 'GET':
+    def get(self, request, format=None):
         foos = Foo.objects.all()
         serializer = FooSerializer(foos, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = FooSerializer(data=data)
+    def post(self, request, format=None):
+        serializer = FooSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def foo_detail(request, pk):
+class FooDetail(APIView):
     """
-    Retrieve, update, or delete a foo.
+    Retrive, update, or delete a foo instance.
     """
-    try:
-        foo = Foo.objects.get(pk=pk)
-    except Foo.DoesNotExist:
-        return HttpResponse(status=404)
+    def get_object(self, pk):
+        try:
+            return Foo.objects.get(pk=pk)
+        except Foo.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
-        serializer = FooSerializer(foo)
-        return JSONResponse(serializer.data)
+        def get(self, request, pk, format=None):
+            foo = self.get_object(pk)
+            serializer = FooSerializer(foo)
+            return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = FooSerializer(foo, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+        def put(self, request, pk, format=None):
+            foo = self.get_object(pk)
+            serializer = FooSerializer(foo, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
 
-    elif request.method == 'DELETE':
-        foo.delete()
-        return HttpResponse(status=204)
+        def delete(self, request, pk, format=None):
+            foo = self.get_object(pk)
+            foo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
